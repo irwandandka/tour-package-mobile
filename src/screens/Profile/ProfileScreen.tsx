93a8083c2c5acaf61c8baf587f93753eb0aacffb
irwandandka <irwndandka@gmail.com>
2025-07-17 23:49:22 +0700

    feat: Profile Page

---
+++ src/screens/Profile/ProfileScreen.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function ProfileScreen({ navigation }: any) {
+    return (
+        <View>
+            <Text>
+                Profile Screen
+            </Text>
+        </View>
+    );
+}
