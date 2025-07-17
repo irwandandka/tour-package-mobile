9184ccf3195776a8cd39d60bd6e58cd4360fc7d4
irwandandka <irwndandka@gmail.com>
2025-07-17 23:49:34 +0700

    feat: Auth

--- App.tsx
+++ App.tsx
@@ -1,13 +1,16 @@
 import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
 import RootNavigator from "./src/navigations/RootNavigator";
-import './i18n';
-
+import "./i18n";
+import { AuthProvider } from "./contexts/AuthContext";
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <RootNavigator />
-    </NavigationContainer>
+    <AuthProvider>
+      {/* The AuthProvider wraps the entire app to provide authentication context */}
+      <NavigationContainer>
+        <RootNavigator />
+      </NavigationContainer>
+    </AuthProvider>
   );
 }
