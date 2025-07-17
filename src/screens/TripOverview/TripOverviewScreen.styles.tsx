fc21cc24f5da6e0ebe9cc4d10a58ab0c1e7c0cc9
irwandandka <irwndandka@gmail.com>
2025-07-17 23:48:42 +0700

    feat: trip overview page

---
+++ src/screens/TripOverview/TripOverviewScreen.styles.tsx
@@ -0,0 +1,84 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+        padding: 21,
+        backgroundColor: '#fff',
+    },
+    headerSection: {
+
+    },
+    headerTitle: {
+
+    },
+    card: {
+
+    },
+    cardLeftSide: {
+
+    },
+    cardImage: {
+
+    },
+    cardRightSide: {
+
+    },
+    cardTitleWrapper: {
+
+    },
+    cardTitle: {
+        
+    },
+    cardRatingWrapper: {
+
+    },
+    cardStarIcon: {
+
+    },
+    cardStarRating: {
+
+    },
+    textNights: {
+
+    },
+
+    // Room Section
+    roomSection: {
+
+    },
+    titleRoomSection: {
+
+    },
+    roomCard: {
+
+    },
+    roomCardHeader: {
+
+    },
+    roomCardHeaderButtonAdd: {
+
+    },
+    roomCardHeaderTitle: {
+
+    },
+    roomCardHeaderAllotment: {
+
+    },
+    roomCardBody: {
+
+    },
+    roomCardBodyImage: {
+
+    },
+    roomCardBodyTitle: {
+    },
+    roomCardBodyInputWrapper: {
+
+    },
+    roomCardBodyInputTitle: {
+
+    },
+    roomCardBodyInput: {
+
+    },
+});
---
+++ src/screens/TripOverview/TripOverviewScreen.tsx
@@ -0,0 +1,14 @@
+import { SafeAreaView } from "react-native-safe-area-context";
+import { View, ScrollView, Text, TouchableOpacity } from "react-native";
+import styles from "./TripOverviewScreen.styles";
+
+export default function TripOverviewScreen({ navigation }: any) {
+    return (
+        <SafeAreaView>
+            <View
+                style={styles.container}>
+                
+            </View>
+        </SafeAreaView>
+    );
+}
